757b8249f65b79d2c36f5ac1f4a5a38b6962dfa3
NatnaelBerhanu-1 <natnaelberhanu18@gmail.com>
2020-03-08 14:10:43 +0300

    beta version finished

--- main.py
+++ main.py
@@ -5,25 +5,32 @@ import time
 
 
 while True:
-    print("going..")
-    resp = requests.get('http://127.0.0.1:8000/api/channels')
-    channels = json.loads(resp.text)
-    channels = channels['data']
-    print("length: " + str(len(channels)))
-    print(channels)
-    for channel in channels:
-        username = channel['username']
-        newchannel = Channel(username)
-        if newchannel.title != channel['title'] or newchannel.subscribers_count != channel['members'] :
-            newchannel = {
-                    'title': newchannel.title,
-                    'description': newchannel.description,
-                    'members': newchannel.subscribers_count,
-                }
-            url = 'http://127.0.0.1:8000/api/channel/' + str(channel['id'])
-            print(url)
-            time.sleep(20)
-            resp = requests.put(url, data=newchannel)
-            print(resp.status_code)
+    try:
+        print("going..")
+        resp = requests.get('http://127.0.0.1:8000/api/channels')
+        channels = json.loads(resp.text)
+        channels = channels['data']
+        print("length: " + str(len(channels)))
+        print(channels)
+        for channel in channels:
+            username = channel['username']
+            print(username)
+            newchannel = Channel('@'+str(username))
+            print(newchannel.valid)
+            if(newchannel.valid == 'valid'):
+                if newchannel.title != channel['title'] or newchannel.subscribers_count != channel['members'] :
+                    newchannel = {
+                            'title': newchannel.title,
+                            'description': newchannel.description,
+                            'members': newchannel.subscribers_count,
+                        }
+                    url = 'http://127.0.0.1:8000/api/channel/' + str(channel['id'])
+                    print(url)
+                    # time.sleep(5)
+                    response = requests.put(url, data=newchannel)
+                    print(response.text)
+    except:
+        print("something wen't wrong continuing")
+        pass
     time.sleep(3600)
 
